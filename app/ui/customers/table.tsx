5aa0cebdb794c937e90ce76ccb6072128de37fec
Vitali Hapankou <vitalihapankou@MacBook-Pro-Vitalij.local>
2026-01-06 13:22:05 +0400

    add customers table

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -5,12 +5,14 @@ import {
   CustomersTableType,
   FormattedCustomersTable,
 } from "@/app/lib/definitions";
+import {fetchCustomers, fetchFilteredCustomers} from "@/app/lib/data";
 
 export default async function CustomersTable({
-  customers,
+  query,
 }: {
-  customers: FormattedCustomersTable[];
+  query: string;
 }) {
+  const customers = await fetchFilteredCustomers(query);
   return (
     <div className="w-full">
       <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
